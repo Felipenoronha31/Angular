14b06f15f49f54f58e7def3fe7fb246a24d98db1
Felipenoronha31 <felipejosenoronha@gmail.com>
2020-09-02 20:05:59 -0300

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component, VERSION } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
+  selector: 'my-app', //<my-app></my-app>
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+  public todos: any[] = [];
+
+  constructor() {
+    this.todos.push({
+      title: 'Passear com o cachorro', 
+      done: false});
+      this.todos.push({
+      title: 'Ir ao supermercado', 
+      done: false});
+  }
 }
